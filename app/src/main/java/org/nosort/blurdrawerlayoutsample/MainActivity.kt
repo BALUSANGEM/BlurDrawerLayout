c84a3cd6a4b4e45bbf0815c0bc7c7e650c172808
bala <vishnubalu.sangem@gmail.com>
2018-08-23 17:32:31 +0530

    Examples with Right and Left drawers

--- app/src/main/java/org/nosort/blurdrawerlayoutsample/MainActivity.kt
+++ app/src/main/java/org/nosort/blurdrawerlayoutsample/MainActivity.kt
@@ -1,82 +1,25 @@
 package org.nosort.blurdrawerlayoutsample
 
-import android.os.Build
+import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.support.v4.app.Fragment
-import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Gravity
 import kotlinx.android.synthetic.main.activity_main.*
-import org.avantari.blurdrawerlayout.dummy.DummyContent
 
 
-class MainActivity : AppCompatActivity(), RecyclerViewFragment.OnListFragmentInteractionListener {
-    override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
-    }
-
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setUpToolbar()
         setUpClickListener()
-        blurDrawerLayout.setScrimColor(getColorCompat(android.R.color.transparent))
-        createFragmentAt(0)
-    }
-
-    private fun createFragmentAt(at: Int) {
-        val fragment = when (at) {
-            0 -> ScrollFragment()
-            1 -> RecyclerViewFragment()
-            2 -> SimpleFragment()
-            else -> null
-        }
-        fragment?.let {
-            blurDrawerLayout.closeDrawer(Gravity.START)
-            Handler().postDelayed({
-                showFragment(fragment)
-            }, 200)
-        }
-    }
-
-    private fun showFragment(fragment: Fragment) {
-        val ft = supportFragmentManager.beginTransaction()
-        ft.replace(R.id.contentLayout, fragment)
-        ft.commit()
     }
 
     private fun setUpClickListener() {
-        menuone.setOnClickListener {
-            createFragmentAt(0)
-        }
-        menuTwo.setOnClickListener {
-            createFragmentAt(1)
-        }
-        menuThree.setOnClickListener {
-            createFragmentAt(2)
-        }
-
+        startGravityActivity.setOnClickListener { gotoActivity(LeftDrawerActivity::class.java) }
+        endGravityActivity.setOnClickListener { gotoActivity(RightDrawerActivity::class.java) }
     }
 
-    lateinit var toggle: ActionBarDrawerToggle
-
-    private fun setUpToolbar() {
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        toggle = ActionBarDrawerToggle(this, blurDrawerLayout, R.string.drawer_open, R.string.drawer_close)
-        toggle.isDrawerIndicatorEnabled = true
-        blurDrawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
+    private fun gotoActivity(className: Class<*>) {
+        startActivity(Intent(this, className))
     }
-
-    override fun onOptionsItemSelected(item: android.view.MenuItem): Boolean {
-        return toggle.onOptionsItemSelected(item)
-    }
-
-    private fun getColorCompat(color: Int): Int =
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                resources.getColor(color, null)
-            } else {
-                resources.getColor(color)
-            }
 }
